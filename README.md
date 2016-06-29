335d7fb70b3da1b40e56addadce16020e79601aa
Thomas Steinholz <tsteinholz@yahoo.com>
2016-06-28 21:57:46 -0400

    getting started

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# AudioVisualizer
+# aVis
+
+This is an Audio Visualizer Application that can stream audio and make a show of
+it.
+
+## Coding Standards
+
+* [Google JavaScript](https://google.github.io/styleguide/javascriptguide.xml)
---
+++ js/aVis.js
@@ -0,0 +1,66 @@
+////////////////////////////////////////////////////////////////////////////////
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 тнσмαѕ ѕтєιинσℓz
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+////////////////////////////////////////////////////////////////////////////////
+
+"use strict";
+
+var gl;
+
+window.onload = function init() {
+  var canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    var vShader = createShader(gl, "gfx/vGalaxy.glsl", gl.VERTEX_SHADER),
+        fShader = createShader(gl, "glx/fGalaxy.glsl", gl.FRAGMENT_SHADER),
+        program = gl.createProgram();
+
+    gl.attachShader(program, vShader);
+    gl.attachShader(program, fShader);
+    gl.linkProgram(program)
+
+    gl.useProgram( program );
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      alert("Could not initialise shaders");
+    }
+
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+
+    // Associate out shader variables with our data buffer
+    var vPos = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPos, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPos );
+
+    render();
+};
+
+fucntion render() {
+  gl.clear( gl.COLOR_BUFFER_BIT );
+  gl.drawArrays( gl.POINTS, 0, points.length );
+};
